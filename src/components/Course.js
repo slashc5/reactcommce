387c5fbcb81629388ba4442dfa9877e68266645b
Benjamin Axel <axel_313@yahoo.com>
2022-09-12 16:03:56 -0300

    agregado de estados

--- src/components/Course.js
+++ src/components/Course.js
@@ -1,10 +1,20 @@
+import { useState } from 'react';
+
 const Course= (props) => {
+    const [almacen, setAlmacen] = useState(0);
+    
+    const handleAlmacen = () => {
+        setAlmacen(almacen+1);
+    }
     return (
         <>
         <h2>{props.name}</h2>
             <p>Anime: {props.category}</p>
             <p>Duracion: {props.genero}</p>
             <hr />
+            <div class="d-grid gap-2 d-md-block">
+  <button class="btn btn-primary" type="button"onClick={handleAlmacen}>almacen</button>
+</div>  {almacen} Stock 
             </>
     )
 }
